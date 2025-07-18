f818e5d792fa5e6b76ca9abc1e5af1e4162426a8
caoyangyi <798188057@qq.com>
2025-07-18 19:48:12 +0800

    button test

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -3,9 +3,16 @@ import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-test("", () =>{
-    const wrapper = render(<Button>Nice</Button>)
-    let element = wrapper.getByText("Nice");
-    expect(element).toBeTruthy();
-    expect(element).toBeInTheDocument();
+
+
+describe('test Button component', () => {
+    it("has the button", () => {
+        const wrapper = render(<Button>Nice</Button>)
+        let element = wrapper.getByText("Nice");
+        expect(element).toBeTruthy();
+        expect(element).toBeInTheDocument();
+        expect(element.tagName).toEqual("BUTTON");
+        expect(element).toHaveClass('btn');
+    });
 });
+
