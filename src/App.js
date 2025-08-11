fe535c313f23dd278c08f10d0948bffa13d81904
caoyangyi <798188057@qq.com>
2025-08-11 22:16:40 +0800

    icon组件

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Tabs from './components/Tabs/tabs';
 import TabItem  from "./components/Tabs/tabItem";
+import Icon from "./components/Icon/icon";
 
 function App() {
   return (
+
     <div className="App">
+        <Icon icon="fa-solid fa-house" size="6x" rotation={90}></Icon>
       <Tabs>
           <TabItem label="Tab 1">11111</TabItem>
           <TabItem label="Tab 2">2222</TabItem>
---
+++ src/components/Icon/icon.tsx
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+import React, { FC } from 'react';
+import classNames from 'classnames';
+import Menu from "../Menu/menu";
+
+type themeType = 'primary' | 'success' | 'info' | 'warning' | 'danger' | 'dark' | 'light';
+export interface IconProps extends FontAwesomeIconProps {
+    className?: string;
+    style?: React.CSSProperties;
+    theme?: themeType;
+}
+
+const Icon: FC<IconProps> = (props) => {
+    const { className, theme, style, ...restProps } = props;
+    const classes = classNames('cyy-icon', className, {
+        [`icon-${theme}`]: theme
+    });
+    return (
+        <FontAwesomeIcon
+            className={classes}
+            style={style}
+            {...restProps}
+        />
+    );
+};
+
+export default Icon;
